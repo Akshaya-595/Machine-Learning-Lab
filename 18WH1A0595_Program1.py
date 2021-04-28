a9d552c6846d5f03caf4967dad58a9745f27e695
Akshaya-595 <69202916+Akshaya-595@users.noreply.github.com>
2021-04-28 15:15:45 +0530

    Add files via upload

---
+++ 18WH1A0595_Program1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+probAbsentFriday = 0.03
+probFriday = 0.2
+
+
+# In[6]:
+
+
+bayesresult=(probAbsentFriday/probFriday)
+a=(bayesresult*100)
+print(a)
+
+
+# In[ ]:
+
+
+
+
